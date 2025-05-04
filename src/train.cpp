e3a68267ca3bfed152ab8a76f88d0cf3f48ee088
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-04 19:26:08 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,38 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <cstdlib>
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+void Train::addCar(bool light) {
+    Car *newCar = new Car{light, nullptr, nullptr};
+    if (!first) {
+        first = newCar;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Car *last = first->prev;
+        last->next = newCar;
+        newCar->prev = last;
+        newCar->next = first;
+        first->prev = newCar;
+    }
+}
+
+int Train::getLength() {
+    if (!first) return 0;
+
+    int length = 0;
+    Car *current = first;
+    do {
+        length++;
+        countOp++;
+        current = current->next;
+    } while (current != first);
+
+    return length;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
