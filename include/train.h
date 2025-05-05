eed5980572b88436c146e52303afeb23606b846e
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-05 19:38:48 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,21 +1,22 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+
 class Train {
  private:
   struct Car {
-    bool hasLight;
+    bool light; // состояние лампочки
     Car *next;
     Car *prev;
   };
-  int operationCount;
-  Car *head;
+  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  Car *first; // точка входа в поезд (первый вагон)
+  Train::Car *create(bool light);
+  Car *last;
  public:
   Train();
-  void addCar(bool hasLight);
-  int getLength();
-  int getOpCount();
-  ~Train();
+  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();          // вычислить длину поезда
+  int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
-
 #endif  // INCLUDE_TRAIN_H_
