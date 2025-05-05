ae05d7ab7c061de3a5e70f6d057b36ae26b98d66
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-05 13:26:58 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,20 +3,21 @@
 
 Train::Train() : operationCount(0), head(nullptr) {}
 
-void Train::addCar(bool light) {
-  Carriage* newCarriage = new Carriage{light, nullptr, nullptr};
-  
+void Train::addCar(bool hasLight) {
+  Car* newCar = new Car{hasLight, nullptr, nullptr};
+
   if (head == nullptr) {
-    head = newCarriage;
+    head = newCar;
     head->next = head;
     head->prev = head;
-  } else {
-    Carriage* tail = head->prev;
-    tail->next = newCarriage;
-    newCarriage->prev = tail;
-    newCarriage->next = head;
-    head->prev = newCarriage;
+    return;
   }
+
+  Car* lastCar = head->prev;
+  lastCar->next = newCar;
+  newCar->prev = lastCar;
+  newCar->next = head;
+  head->prev = newCar;
 }
 
 int Train::getOpCount() {
@@ -25,34 +26,34 @@ int Train::getOpCount() {
 
 int Train::getLength() {
   if (head == nullptr) return 0;
-  
-  int Train::getLength() {
-  if (head == nullptr) return 0;
 
-  int countCarriages = 0;
-  Carriage* current = head;
+  operationCount = 0;
+  int carCount = 0;
+  Car* currentCar = head;
 
   do {
-    if (!current->light) {
-      countCarriages++;
+    if (!currentCar->hasLight) {
+      carCount++;
+      currentCar->hasLight = true;
     }
-    current = current->next;
-    operationCount++;
-  } while (current != head);
-  return countCarriages;
+    currentCar = currentCar->next;
+    operationCount += 2;
+  } while (currentCar != head);
+
+  return carCount;
 }
 
 Train::~Train() {
   if (head == nullptr) return;
-  
-  Carriage* current = head;
-  Carriage* nextCarriage;
+
+  Car* currentCar = head;
+  Car* nextCar;
 
   do {
-    nextCarriage = current->next;
-    delete current;
-    current = nextCarriage;
-  } while (current != head);
-  
+    nextCar = currentCar->next;
+    delete currentCar;
+    currentCar = nextCar;
+  } while (currentCar != head);
+
   head = nullptr;
 }
